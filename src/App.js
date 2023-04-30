dbaf2c400b83b31936eefe61bb18241023996469
MeghanaSreepada <sreepadameghana@gmail.com>
2023-04-29 18:00:03 -0700

    changes in the courses taken

--- src/App.js
+++ src/App.js
@@ -1,34 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import CourseList from './CourseList';
 import './App.css';
 
 function App() {
-  const courses = [
-    {
-      id: 1,
-      title: 'React Basics',
-      description: 'Learn the basics of React',
-    },
-    {
-      id: 2,
-      title: 'React Hooks',
-      description: 'Learn how to use React Hooks',
-    },
-    {
-      id: 3,
-      title: 'React Router',
-      description: 'Learn how to use React Router',
-    },
-  ];
+  const [courses, setCourses] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await fetch('http://localhost:5000/api/courses');
+        const data = await response.json();
+        setCourses(data);
+        setIsLoading(false);
+      } catch (error) {
+        setError(error);
+        setIsLoading(false);
+      }
+    }
+    fetchData();
+  }, []);
 
   return (
     <div className="App">
       <h1>My Courses</h1>
-      <CourseList courses={courses} />
+      <CourseList courses={courses} isLoading={isLoading} error={error} />
     </div>
   );
 }
 
 export default App;
+
+
 
 
